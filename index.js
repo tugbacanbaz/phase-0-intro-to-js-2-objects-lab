eb93b8a2881bd55aaae2f4933c6029cb86d19bde
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-11-16 19:23:28 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee={
+    name:'Sam',
+    streetAddress:'12 Broadway',
+};
+
+const updateEmployeeWithKeyAndValue=(employee, key, value)=>{
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue=(employee, key, value)=>{
+    employee[key] = value;
+    return employee;
+}
+
+const deleteFromEmployeeByKey=(employee, key)=>{
+    const newEmployee={...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+const destructivelyDeleteFromEmployeeByKey=(employee, key)=>{
+    delete employee[key];
+    return employee;
+}
